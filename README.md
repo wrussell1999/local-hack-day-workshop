0d18e9b807a9ed3d6aa11841e0931a5f28bae1bb
Will Russell <jetpowerss@gmail.com>
2019-09-30 02:31:02 +0100

    Basic bot working with subreddits
    Using command picture <subreddit name> will get the first 25 posts of a subreddit and save the URLs of posts with images. One of these images is then posted at random into the same channel.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# discord-bot-workshop
-Local Hack Day - Learn
+# Reddit Scraping Discord Bot Workshop
+
+[Local Hack Day - Learn workshop at the University of Birmingham](https://localhackday.mlh.io/learn/locations/1859) run by Will Russell.
+
+## Setup
+
+```bash
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
---
+++ discord_bot/bot.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import praw
+import requests
+import random
+from io import BytesIO
+from . import config
+
+bot = commands.Bot(command_prefix='!')
+reddit = praw.Reddit(client_id=config.creds['reddit_client_id'],
+                     client_secret=config.creds['reddit_client_secret'],
+                     user_agent=config.creds['reddit_user_agent'])
+
+def main():
+    bot.run(config.creds['discord_token'])
+
+def get_subreddit_image(name):
+    subreddit = reddit.subreddit(name)
+    urls = []
+    for submission in reddit.subreddit(name).hot(limit=25):
+        if submission.domain == 'i.redd.it':
+            urls.append(submission.url)
+    
+    response = requests.get(random.choice(urls))
+    image = BytesIO(response.content)
+    return image
+
+@bot.event
+async def on_ready():
+    print('Ready!')
+
+@bot.command(description="Post a picture from a subreddit")
+async def picture(ctx):
+    theme = ctx.message.clean_content.split(' ')[1]
+    image = get_subreddit_image(theme)
+    await ctx.send(file=discord.File(image, 'image.png'))
